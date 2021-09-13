e48024be9d9cd5e66492e5a68e2497837106dd4e
duslerke <liudvikast@gmail.com>
2021-09-13 16:27:18 +0100

    Databases script: made it find the file names of the EFCore (due to standards implies PostgreSql) and MongoDB Database Contexts.

--- scrapers/databases.sh
+++ scrapers/databases.sh
@@ -10,3 +10,18 @@ while getopts 'd:' flag; do
 done
 
 echo "hello! dirpath: ${projectDirectory}"
+
+# No need to search the project root or tests project
+apiProjectDirectory=$( find $projectDirectory -mindepth 1 -type d -iname '*Api' )
+
+# echo "something: ${apiProjectDirectory}"
+
+postgreContextsList=$( grep -rnwE $apiProjectDirectory -e 'public class \w+ : DbContext' ) #'public\sclass\s\w+\s:\sDbContext'
+
+echo -e "Result: ${postgreContextsList}"
+
+mongoContextsList=$( grep -rlwE $apiProjectDirectory -e 'public IMongoCollection<BsonDocument> \w+ { get; set; }' )
+
+echo -e "Mongo: ${mongoContextsList}"
+
+#'public class DatabaseContext : DbContext'
