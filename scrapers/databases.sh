9fca856ac3b3e60c767514968839ad000ea6e662
duslerke <liudvikast@gmail.com>
2021-09-21 23:25:45 +0100

    DB Script: List All Controller method data

--- scrapers/databases.sh
+++ scrapers/databases.sh
@@ -14,6 +14,8 @@ done
 # If incorrect args get provided, this means there's an issue with
 # how the project is structured, so it's probably for the better to
 # exit entirely rather than to give a false positive impression.
+# This function should be used for more general cases, for MVP only
+# we assume typical code practices
 function find_class_interface {
     local className=$1
 	local startupFilePath=$2
@@ -42,10 +44,17 @@ function get_file_interface {
 
     if [ -z "$interfaceName" ]
     then
-        echo "No interface was found!" && exit 1
+        echo "No interface was found!" && exit 1 # Exit code is used in a way, where if command succeeds finding GW, the proceeds UC
     else
         echo $interfaceName
     fi
+}
+
+function find_files_using_interface {
+    local interface=$1
+
+    [ -z "$interface" ] && { echo "Interface is empty!"; exit 1; }
+    
 }
 
 
@@ -77,16 +86,73 @@ do
     # the use cases that use this gateway as their dependency
     # TODO: Modify this to work with multiple inheritance (like : A, B)
     gatewayInterface=$( get_file_interface $gateways )
-    echo "GWI: $gatewayInterface"
+    #echo "GWI: $gatewayInterface"
     # Finds UC and another GW???
     usecases=$( grep -rlwP $apiProjectDirectory -e "(?<=private readonly )$gatewayInterface(?= \w+;)" | grep -P ".+?\/V\d\/UseCase\/.+" )
     for ucFile in $usecases
     do
         # TODO: extract this Interface extraction command into a function - it's going to get used many times
         ucInterface=$( get_file_interface $ucFile )
-        echo "uc: $ucInterface"
+        #echo "uc: $ucInterface"
     done
 done
 
+echo "Start!"
+
+dependencyInterfacePattern='(?<=private readonly )\w+(?= \w+;)'
+dependencyVariablePattern='(?<=private readonly )\w+ \K\w+(?=;)'
+
+dependencyPattern='(?<=private readonly )\w+ \w+(?=;)'
+
+endpointTypePattern='(?<=[Http)\w+'
+endpointName='(IActionResult>? \K\w+)'
+
+
+# Let's try going from the other side instead:
+# For MVP version is hard coded to V1, TODO: make script search versions first
+# Get the list of Controllers:
+controllersList=$( find "$apiProjectDirectory/V1/Controllers" -mindepth 1 )
+
+for controller in $controllersList
+do
+    echo -e "\n$controller"
+    # usecasesList=$( grep -oP -e "$dependencyPattern" $contoller )
+    # for usecase in $usecasesList
+    # do
+    #     echo $usecase
+    # done
+    # endpointsList=$( pcregrep -oM '\[Http\w+\]\s+(?:\[[^\[\]]+\]\s+)*public (?:async Task<)?IActionResult>? \w+' $controller | perl -pe 's/\[Http(\w+)\]\s+(?:\[[^\[\]]+\]\s+)*public (?:async Task<)?IActionResult>? (\w+)/$1~$2/g' )
+
+    # for endpoint in $endpointsList
+    # do
+    #     echo -e '\n'
+    #     echo -e $endpoint
+    # done
+
+    # echo $endpointsList
+    pcregrep -oM '\[Http\w+\]\s+(?:\[[^\[\]]+\]\s+)*public (?:async Task<)?IActionResult>? \w+' $controller | \
+    perl -0777 -pe 's/(?:(?:\[Http(\w+)\]|\[Route\(\"([^\[\(\)\]]+)\"\)\])\s+)+(?:\[[^\[\]]+\]\s+)*public (?:async Task<)?IActionResult>? (\w+)/R: \2\; T: \1\; N: \3\;/gm' #'s/(?:(?:\[Http(\w+)\]|\[Route\(\"([^\[\(\)\]]+)\"\)\])\s+)+(?:\[[^\[\]]+\]\s+)*public (?:async Task<)?IActionResult>? (\w+)/Type\: \1; Route: \2; Name: \3;/gm' #'s/\[Http(\w+)\]\s+(?:\[[^\[\]]+\]\s+)*public (?:async Task<)?IActionResult>? (\w+)/\1 \2/g'
+
+    #cat $controller | perl -pe 's/\n//g' |
+    #echo -e $usecasesList
+done
+
+
+
+
+#Will need to ignore empty ones like Healthcheck
+
 
 echo "Done!"
+
+# DB Context --> Find Interface if any
+# DB Context -->                       --> Find 
+
+# declare -A arr
+
+# arr["key1"]=val1
+# arr+=( ["key2"]=val2 ["key3"]=val3 )
+
+# for key in ${!arr[@]}; do
+#     echo ${key} ${arr[${key}]}
+# done
